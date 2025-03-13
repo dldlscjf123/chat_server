efa2b710cabda23bc74d3e7343cb26ec7912e378
dldlscjf123 <dldlscjf1231@naver.com>
2025-03-13 14:16:15 +0900

    feat : Member Member 엔티티 개발

---
+++ src/main/java/com/example/chatserver/member/domain/Member.java
@@ -0,0 +1,30 @@
+package com.example.chatserver.member.domain;
+
+import com.example.chatserver.common.domain.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@AllArgsConstructor // 모든 멤버변수 포함 생성자
+@NoArgsConstructor //기본 생성자
+@Getter
+public class Member extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private String password;
+    @Enumerated(EnumType.STRING)
+    @Builder.Default
+    private Role role = Role.USER;
+
+}
