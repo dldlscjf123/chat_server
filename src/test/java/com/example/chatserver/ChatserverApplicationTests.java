4df65e40ded4832de3ead03ff61be8c5a1f5764d
dldlscjf123 <dldlscjf1231@naver.com>
2025-03-13 14:36:05 +0900

    first commit

---
+++ src/test/java/com/example/chatserver/ChatserverApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.chatserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatserverApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+
+}
