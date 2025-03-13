76710b6ea5cb364b8646a986d784cd3e8e083260
dldlscjf123 <dldlscjf1231@naver.com>
2025-03-13 14:30:19 +0900

    feat : Chat Dto

---
+++ src/main/java/com/example/chatserver/chat/dto/ChatMessageDto.java
@@ -0,0 +1,17 @@
+package com.example.chatserver.chat.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ChatMessageDto {
+    private String message;
+    private String senderEmail;
+    private Long roomId;
+
+}
